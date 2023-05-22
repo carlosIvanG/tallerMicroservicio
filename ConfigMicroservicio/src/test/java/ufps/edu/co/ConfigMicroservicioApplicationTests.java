6d93a3bffc92e16196a611e1405ec8722a0d975a
carlosIvanG <carlosivangc@ufps.edu.co>
2023-05-22 16:46:51 -0500

    agregando config-service

---
+++ ConfigMicroservicio/src/test/java/ufps/edu/co/ConfigMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package ufps.edu.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
